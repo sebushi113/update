8c809af02f71eea0ab0b9101e39b467327a0376f
sebushi113 <sebushi113@users.noreply.github.com>
2022-10-11 09:07:48 -0400

    make it better

--- api/hello.js
+++ api/hello.js
@@ -1,11 +1,11 @@
 // `/api/hello.js`
-// export default function hello(req, res) {
-//   res.statusCode = 200;
-//   res.json({ message: "test" });
-// }
+export default function hello(req, res) {
+  res.statusCode = 200;
+  res.json({ message: "test" });
+}
 
-module.exports = (req, res) => {
-  res.json({
-    hi: "test",
-  });
-};
+// module.exports = (req, res) => {
+//   res.json({
+//     hi: "test",
+//   });
+// };
