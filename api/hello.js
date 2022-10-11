cdfbff89f81f1b7be6215f275bf70d8a0a4a4818
sebushi113 <sebushi113@users.noreply.github.com>
2022-10-11 09:15:51 -0400

    make it better

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,17 @@
+// export default function handler(request, response) {
+//   response.status(200).json({
+//     body: request.body,
+//     query: request.query,
+//     cookies: request.cookies,
+//   });
+// }
+
+import * as notify from "../notify";
+
 // `/api/hello.js`
-export default function hello(req, res) {
+export default async function hello(req, res) {
   res.statusCode = 200;
+  await notify.sendMessage();
   res.json({ message: "test" });
 }
 
