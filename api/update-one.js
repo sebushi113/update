a97aa52a5b3823e3ab6e46ef4293a6be1838b070
sebushi113 <sebushi113@users.noreply.github.com>
2022-10-11 22:55:58 -0400

    make it better

--- api/update-one.js
+++ api/update-one.js
@@ -355,7 +355,7 @@ async function append(cpu4_cs1d, cpu4_cd3d, cs1_staked) {
   }
 }
 
-export async function run() {
+async function run() {
   console.log(Date());
   // await cpu4_ub();
   // await sleep(3000);
