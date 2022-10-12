236aff71b069bcd82283ded46b020e41a6bebe00
sebushi113 <sebushi113@users.noreply.github.com>
2022-10-11 22:53:08 -0400

    make it better

--- api/update-one.js
+++ api/update-one.js
@@ -7,7 +7,7 @@ import moment from "moment";
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config();
 // import * as notify from "./notify.js";
-// import { google } from "googleapis";
+import { google } from "googleapis";
 import { sendMessage } from "./notify.js";
 
 const cs1a = process.env.cs1a;
