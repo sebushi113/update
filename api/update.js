3678a57e0ccfb76bd8411e23e4f310c00c9ce682
sebushi113 <sebushi113@users.noreply.github.com>
2022-10-11 22:37:52 -0400

    make it better

--- api/update.js
+++ api/update.js
@@ -1,12 +1,11 @@
-import { sendMessage } from "./notify.js";
+// import { sendMessage } from "./notify.js";
 import { run } from "./update_cpu4.js";
 const chat_id2 = process.env.chat_id2;
 const message = "hello cron test";
 
 export default async function update(req, res) {
-  // await sendMessage(chat_id2, message);
   await run();
-  // res.statusCode = 200;
+  res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
   res.send("cpu4 updated");
 }
