f79ef85c501620496d6a709bea6c08aae4531e57
sebushi113 <sebushi113@users.noreply.github.com>
2022-10-17 17:34:34 -0400

    make it better

--- api/notify.js
+++ api/notify.js
@@ -6,7 +6,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 
 export async function sendMessage(chat_id, error) {
   await bot.telegram.sendMessage(chat_id, error, {
-    parse_mode: "MarkdownV2",
+    parse_mode: "HTML",
     disable_web_page_preview: true,
   });
 }
